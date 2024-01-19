ab968f9ada60af45d594c1ba17d1252d9bdfe5ad
mageshkrishna <gmageshkrishna2003@gmail.com>
2024-01-19 11:59:19 +0530

    completed basic

--- components/customerhomepage/Customersearchbar.js
+++ components/customerhomepage/Customersearchbar.js
@@ -1,184 +1,340 @@
-import { StyleSheet, Text, View, Dimensions, Image, ImageBackground, TouchableOpacity, Linking } from 'react-native'
-import React from 'react'
-import { FontAwesome5, AntDesign } from '@expo/vector-icons';
-import { MaterialIcons } from '@expo/vector-icons';
-import { ScrollView } from 'react-native'
-import { TextInput } from 'react-native'
-import { Feather, Entypo, Ionicons } from '@expo/vector-icons';
-import { Alert } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-const height = Dimensions.get("window").height
-const width = Dimensions.get("window").width
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
+import React, { useState } from "react";
+import { FontAwesome5, AntDesign } from "@expo/vector-icons";
+import { MaterialIcons } from "@expo/vector-icons";
+import { ScrollView } from "react-native";
+import { TextInput } from "react-native";
+import { Feather, Entypo, Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
+// import axios from "axios";
+import { Alert } from "react-native";
+import { SelectList } from "react-native-dropdown-select-list";
+import Checkbox from "expo-checkbox";
+import axios from "axios";
+const height = Dimensions.get("window").height;
+const width = Dimensions.get("window").width;
 
 const CustomerSearchBar = () => {
+  const [businessname, setbusinessname] = useState(null);
+  const [category, setcategory] = useState(null);
+  const [availabilitystatus, setavailabilitystatus] = useState(false);
+  const[shopowner,setshopowner]  = useState([])
+  const storeresult = [
+    {
+      storename: "Anbu",
+      location: "Vallioor",
+      deliverystatus: true,
+    },
+    {
+      storename: "Ram Stores",
+      location: "Petharangapuram",
+      deliverystatus: false,
+    },
+  ];
+  const categories = [
+    { key: "1", value: "grocery" },
+    { key: "2", value: "electronic" },
+    { key: "3", value: "Computers" },
+  ];
+
+  const fetchData = async () => {
+    console.log("searching....")
+    if(!businessname && !category){
+      Alert.alert('businessname required')
+      return;
+    }
+    try {
+      let formdata = {
+        businessname: businessname,
+      };
+  
+      if (category) {  // Corrected the typo from catgeory to category
+        formdata.category = category;
+      }
+  
+      if (availabilitystatus) {
+        formdata.availabilitystatus = availabilitystatus;
+      }
+  
+      const response = await axios.get('https://direckt-copy1.onrender.com/shopowner/getshops', {
+        params: formdata,
+      });
+  
+      console.log(response.data);
+      setshopowner(response.data)
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
+
+  const handleviewprofile = async () => {
+    console.log("Profile page");
+  };
   return (
-    <SafeAreaView style={{flex:1}}>
-    <ScrollView
-      style={styles.container}
-    >
-      <View
-        style={styles.searchcontainer}
-      >
-        <View
-          style={styles.searchbar}
-        >
+    <View style={styles.container}>
+      <View style={styles.searchcontainer}>
+        <View style={styles.searchbar}>
           <Ionicons name="arrow-back" size={24} color="black" />
           <TextInput
             style={styles.searchinput}
-            placeholder={'Search shops near you...'} />
-          <Feather name="search" size={30} color="black" />
+            placeholder={"Search shops near you..."}
+            value={businessname}
+            onChangeText={(e) => setbusinessname(e)}
+            onSubmitEditing={fetchData}
+          />
+          <TouchableOpacity onPress={fetchData}>
+            <Feather name="search" size={30} color="black" />
+          </TouchableOpacity>
         </View>
-        <View
-          style={styles.filtercontainer}
-        >
-          <View style={styles.filter}>
-            <Ionicons name="filter" size={16} color="black" style={{ paddingHorizontal: 3 }} />
-            <Text>filter</Text>
+        <View style={styles.filtercontainer}>
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "center",
+              justifyContent: "flex-start",
+              columnGap: 10,
+            }}
+          >
+            <Text style={{ color: "grey" }}>Shop open</Text>
+            <Checkbox
+              value={availabilitystatus}
+              onValueChange={setavailabilitystatus}
+              color={availabilitystatus ? "#4630EB" : undefined}
+            />
           </View>
-          <View style={styles.location}>
-            <Entypo name="location" size={16} color="black" style={{ paddingHorizontal: 7 }} />
-            <Text>Select Location</Text>
+          <View style={{ width: "45%" }}>
+            <SelectList
+              setSelected={(val) => setcategory(val)}
+              data={categories}
+              save="value"
+              placeholder="category"
+            />
           </View>
         </View>
       </View>
       <ScrollView style={styles.resultcontainer}>
-        {<View style={styles.resultcard}>
-          <View style={styles.storeprofileImage}>
-            <Image
-              source={require('../../assets/splash.png')}
-              style={{ height: '100%', width: '80%', borderTopLeftRadius: 20, borderBottomLeftRadius: 20 }}
-            ></Image>
-          </View>
-          <View style={styles.storedetails}>
-            <Text style={styles.storename} numberOfLines={1}>Anbu Stores</Text>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-              <Ionicons name="location-sharp" size={17} color="black" />
-              <Text style={styles.storelocation} numberOfLines={1}> Vallior</Text>
+        {shopowner.map((data) => {
+          return (
+            <View
+            style={{
+              flexDirection: "row",
+              width: "100%",
+              height: 150,
+              borderStyle:'solid',
+              borderWidth:1,
+              borderColor:'#5271FF',
+              padding:2,
+              borderRadius:5,
+              marginBottom:20
+            
+            }}
+          >
+            <View style={{ width: "30%", height: "100%" }}>
+              <View style={{ height: 100, width: "100%" }}>
+                <Image
+                  source={{ uri: data.profilepic }}
+                  style={{
+                    height: 100,
+                    width: "100%",
+      
+                    borderRadius: 4,
+                  }}
+                ></Image>
+              </View>
+              <TouchableOpacity>
+                <View
+                  style={{ height:50,justifyContent: "center", alignItems: "center" }}
+                >
+                  <Text style={{color:'#5271FF'}}>View Profile</Text>
+                </View>
+              </TouchableOpacity>
             </View>
-            <TouchableOpacity
-              onPress={() => Alert.alert('Profile')}
+            <View
+              style={{
+                paddingHorizontal: 10,
+                width: "70%",
+                height: "100%",
+                
+                rowGap: 5,
+              }}
             >
-              <Text style={styles.storeprofilelink}>View Profile</Text>
-            </TouchableOpacity>
+              <Text  style={{color:'#5271FF'}} >{data.businessname}</Text>
+              <View style={{ height: 63,borderRadius:5}}>
+               <Text style={{marginBottom:10}}>{data.businessabout}</Text>
+               <Text >category: {data.category}</Text>
+              </View>
+              <View
+                style={{
+                  height: 50,
+                
+                  flexDirection: "row",
+                }}
+              >
+                <View
+                  style={{
+                 
+                    width: "50%",
+                    justifyContent: "center",
+                    alignItems: "center",
+                  }}
+                >
+                {data.availabilitystatus?<Text style={styles.storeavailable}>Store is Open</Text>:<Text style={styles.storenotavailable}>Store Closed</Text>}
+                </View>
+                <View
+                  style={{
+                  
+                    width: "50%",
+                    flexDirection: "row",
+                    justifyContent: "space-evenly",
+                    alignItems: "center",
+                    gap: 10,
+                  }}
+                >
+                  <TouchableOpacity
+                    onPress={() => Alert.alert("Hello")}
+                    style={styles.storedirection}
+                  >
+                    <MaterialIcons name="phone-in-talk" size={33} color="#5271FF" />
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    onPress={() => Alert.alert("Hello")}
+                    style={styles.storedirection}
+                  >
+                    <Entypo name="location" size={33} color="#5271FF" />
+                   
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </View>
           </View>
-          <View style={styles.storectc}>
-            <TouchableOpacity
-              onPress={() => Alert.alert('calling...')}
-              style={styles.storedirection}>
-              <MaterialIcons name="phone-in-talk" size={35} color="#5271FF" />
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => Alert.alert('you have reached your destination')}
-              style={styles.storedirection}>
-              <FontAwesome5 name="directions" size={35} color="#5271FF" />
-            </TouchableOpacity>
-          </View>
-        </View>}
+          );
+        })}
       </ScrollView>
-    </ScrollView>
-    </SafeAreaView>
-  )
-}
+    </View>
+  );
+};
 
-export default CustomerSearchBar
+export default CustomerSearchBar;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: '100%',
   },
   searchcontainer: {
-    flex: 1,
-    margin: 0,
-    height: (height * 24) / 100,
+    paddingTop: 50,
     padding: 20,
-    justifyContent: 'space-evenly',
-    elevation: 0.5,
+    justifyContent: "space-evenly",
+    gap: 20,
   },
   searchbar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '35%',
-    backgroundColor: 'white',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 50,
+    backgroundColor: "white",
     elevation: 5,
     borderRadius: 10,
     shadowOffset: {
       height: 2,
-      width: 2
+      width: 2,
     },
     shadowColor: "black",
   },
   searchinput: {
-    height: '100%',
-    width: '75%',
-    paddingHorizontal:9,
+    height: "100%",
+    width: "75%",
+    paddingHorizontal: 9,
   },
   filtercontainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly'
+    flexDirection: "row",
+    justifyContent: "space-evenly",
   },
   filter: {
-    flexDirection: 'row',
-    padding: 6,
+    flexDirection: "row",
     borderRadius: 10,
     borderWidth: 1,
-    borderColor: 'grey',
+    borderColor: "grey",
+    width: "45%",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  location: {
-    flexDirection: 'row',
-    padding: 6,
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: 'grey',
-  },
+
   resultcontainer: {
     flex: 1,
     height: (height * 76) / 100,
     padding: 20,
-    backgroundColor: '#FAF9F9',
+    backgroundColor: "#FAF9F9"
+    
   },
   resultcard: {
     flex: 1,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    height: (height * 13) / 100,
-    backgroundColor: 'white',
-    borderRadius: 20,
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    height: 100,
+    backgroundColor: "white",
+    borderRadius: 10,
     elevation: 1,
-    marginVertical: '2%',
+    marginVertical: "2%",
   },
   storeprofileImage: {
     flex: 1,
-    width: '10%',
-    height: '100%',
+    width: "10%",
+    height: "100%",
   },
   storedetails: {
     flex: 1,
-    justifyContent: 'space-evenly',
-    height: '100%',
-    width: '50%',
+    justifyContent: "space-evenly",
+    height: "100%",
+    width: "50%",
   },
   storectc: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    alignItems: 'center',
-    height: '100%',
-    width: '40%',
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    height: "100%",
+    width: "40%",
   },
   storename: {
     fontSize: 20,
-    fontWeight: 'medium',
+    fontWeight: "medium",
+  },
+  storeavailable: {
+    padding: 4,
+    backgroundColor: "#FAF9F9",
+    borderRadius: 5,
+    fontSize: 12,
+    color: "#00BF63",
+  },
+  storenotavailable: {
+    padding: 4,
+    backgroundColor: "#FAF9F9",
+    borderRadius: 5,
+    fontSize: 12,
+    color: "red",
   },
   storeprofilelink: {
-    color: '#46CC6B',
+    color: "orange",
+    textDecorationLine: "underline",
+  },
+  storelocation: {
+    fontSize: 12,
   },
   storedirection: {
     borderWidth: 2,
     padding: 4,
-    borderColor: '#5271FF',
+    borderColor: "#5271FF",
     borderRadius: 150,
-  }
-})
+  },
+});
--- components/customerhomepage/Threadsavailable.js
+++ components/customerhomepage/Threadsavailable.js
@@ -1,29 +1,374 @@
-import { View, Text, StyleSheet} from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import { useNavigation } from '@react-navigation/native'
-import { COLORS } from '../../constants/Theme'
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+  LayoutAnimation,
+  UIManager,
+  Image,
+  ImageBackground,
+  Pressable,
+  Linking,
+  Alert,
+} from "react-native";
+import { useState, useEffect } from "react";
+import {
+  MaterialIcons,
+  AntDesign,
+  Ionicons,
+  FontAwesome5,
+} from "@expo/vector-icons";
+import React from "react";
+import axios, { Axios } from "axios";
+import { Entypo } from '@expo/vector-icons';
+const height = Dimensions.get("window").height;
+const width = Dimensions.get("window").width;
+
+UIManager.setLayoutAnimationEnabledExperimental &&
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+const InsideAccorditon = ({ data }) => {
+  console.log(" InsideAccorditon " + data.shopowner_id.businessname);
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        width: "100%",
+        height: 150,
+        borderStyle:'solid',
+        borderWidth:1,
+        borderColor:'#5271FF',
+        padding:2,
+        borderRadius:5
+      
+      }}
+    >
+      <View style={{ width: "30%", height: "100%" }}>
+        <View style={{ height: 100, width: "100%" }}>
+          <Image
+            source={{ uri: data.shopowner_id.profilepic }}
+            style={{
+              height: 90,
+              width: "100%",
+
+              borderRadius: 4,
+            }}
+          ></Image>
+        </View>
+        <TouchableOpacity>
+          <View
+            style={{ justifyContent: "space-evenly", alignItems: "center" }}
+          >
+            <Text>{data.shopowner_id.businessname}</Text>
+
+            <Text style={{color:'#5271FF'}}>View Profile</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          paddingHorizontal: 10,
+          width: "70%",
+          height: "100%",
+          
+          rowGap: 5,
+        }}
+      >
+        <Text  style={{color:'#5271FF'}} >Reply message:</Text>
+        <View style={{ height: 63,borderRadius:5}}>
+          <ScrollView style={{ borderRadius: 2,borderWidth:0.1 }}>
+            <Text style={{padding:5}}>
+              {data.replymessage}
+            </Text>
+          </ScrollView>
+        </View>
+        <View
+          style={{
+            height: 50,
+          
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+           
+              width: "50%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {data.deliveryStatus ? (
+              <Text>Delivery:<Text style={{color:'green'}}> YES</Text></Text>
+            ) : (
+              <Text>Delivery:<Text  style={{color:'red'}}> NO</Text></Text>
+            )}
+          </View>
+          <View
+            style={{
+            
+              width: "50%",
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              alignItems: "center",
+              gap: 10,
+            }}
+          >
+            <TouchableOpacity
+              onPress={() => Alert.alert("Hello")}
+              style={styles.storedirection}
+            >
+              <MaterialIcons name="phone-in-talk" size={33} color="#5271FF" />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => Alert.alert("Hello")}
+              style={styles.storedirection}
+            >
+              <Entypo name="location" size={33} color="#5271FF" />
+             
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+const AccordionItem = ({ data }) => {
+  const [expanded, setExpanded] = useState(false);
+  const [jobreply, setjobreply] = useState([]);
+
+  useEffect(() => {
+    setjobreply(data.jobreply);
+    console.log(jobreply);
+  }, [data]);
+  const toggleExpand = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    setExpanded(!expanded);
+  };
+
+  return (
+    <View>
+      <Pressable onPress={toggleExpand}>
+        
+        <View style={styles.thread}>
+          <TouchableOpacity
+            style={styles.threadImage}
+            // onPress={() => setShowPopup(true)}
+          >
+            <Image
+              source={{ uri: data.image_url }}
+              style={{
+                height: "100%",
+                width: "100%",
+                backgroundColor: "white",
+              }}
+            />
+            <View style={{ flexDirection: "row", alignItems: "center" }}>
+              <MaterialIcons name="category" size={17} color="black" />
+              <Text style={styles.threadcategory} numberOfLines={1}>
+                {data.category}
+              </Text>
+            </View>
+          </TouchableOpacity>
+          <Pressable style={styles.threaddetails} onPress={toggleExpand}>
+            <Text style={styles.threadtitle} numberOfLines={2}>
+              {data.jobtitle}
+            </Text>
+            <Text style={styles.threaddes} numberOfLines={3}>
+              {data.jobdescription}
+            </Text>
+            <View
+              style={{
+                flexDirection: "row",
+                marginTop: 3,
+                alignItems: "center",
+                justifyContent: "space-evenly",
+              }}
+            >
+              <TouchableOpacity
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  justifyContent: "space-evenly",
+                }}
+                onPress={() => Alert.alert("Do you want to delete?")}
+              >
+                <AntDesign name="delete" size={12} color="red" />
+                <Text style={styles.threadowner}> Delete</Text>
+              </TouchableOpacity>
+              <View style={styles.viewdetails}>
+                <Text>View response</Text>
+              </View>
+            </View>
+          </Pressable>
+        </View>
+      </Pressable>
+      {expanded && jobreply && jobreply.length > 0 && (
+        <View style={styles.responsecontainer}>
+          <Text
+            style={{ textAlign: "center", paddingVertical: 10, fontSize: 18 }}
+          >
+            job Responses
+          </Text>
+          {jobreply.map((item, index) => (
+            <InsideAccorditon key={index} data={item} />
+          ))}
+        </View>
+      )}
+    </View>
+  );
+};
 
 const Threadsavailable = () => {
-  const navigation = useNavigation();
-  const Handlenavigatetocreatethread = () =>{
-    navigation.navigate('Createthread')
+  const [data, setdata] = useState([]);
+  const v = false;
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://direckt-copy1.onrender.com/Customerdata/getreplydata?email=magesh@gmail.com"
+        );
+        console.log(response.data);
+        setdata(response.data.result);
+        console.log(data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [v]);
+  if (data.length == 0) {
+    return (
+      <View>
+        <View style={styles.titlecontainer}>
+          <MaterialIcons name="live-tv" size={24} color="green" />
+          <Text style={styles.pagetitle}>Your Threads</Text>
+        </View>
+        <Text>Loading...</Text>
+      </View>
+    );
   }
   return (
-    <View style={{flex:1 ,alignItems:'center',justifyContent:'center'}}>
-      <TouchableOpacity onPress={Handlenavigatetocreatethread} style={styles.nav} >
-        <Text style={{color:'white',fontSize:16,fontWeight:500}}>Create your First Thread</Text>
-      </TouchableOpacity>
-    </View>
-  )
-}
+    <ScrollView>
+    
+      <ScrollView style={styles.threadcontainer}>
+        <View>
+          {data.map((item, index) => (
+            <AccordionItem key={index} data={item} />
+          ))}
+        </View>
+      </ScrollView>
+    </ScrollView>
+  );
+};
 
-export default Threadsavailable
-const styles= StyleSheet.create({
-  nav:{
-    paddingHorizontal:20,
-    paddingVertical:10,
-    backgroundColor:COLORS.primary,
-    borderRadius:10
-  }
-})
+export default Threadsavailable;
+
+const styles = StyleSheet.create({
+  titlecontainer: {
+    flex: 1,
+    flexDirection: "row",
+    height: (height * 5) / 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  threadcontainer: {
+    flex: 1,
+    height: (height * 85) / 100,
+    paddingHorizontal: 10,
+    backgroundColor: "#E0E5FF",
+  },
+  thread: {
+    flex: 1,
+    alignItems: "center",
+    flexDirection: "row",
+    height: (height * 18) / 100,
+    backgroundColor: "white",
+    elevation: 1,
+    marginVertical: "3%",
+    marginHorizontal: "3%",
+    borderRadius: 5,
+  },
+  threadImage: {
+    width: (width * 35) / 100,
+    height: "70%",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+  },
+  threadcategory: {
+    paddingVertical: 5,
+  },
+  threaddetails: {
+    flex: 1,
+    height: "100%",
+    justifyContent: "space-evenly",
+    width: (width * 65) / 100,
+    padding: 7,
+  },
+  threadsection: {
+    flex: 1,
+    justifyContent: "space-evenly",
+    alignItems: "flex-start",
+    marginRight: 5,
+    height: "100%",
+    width: "40%",
+  },
+  threadtitle: {
+    fontSize: 18,
+    fontWeight: "medium",
+  },
+  threaddes: {
+    fontSize: 13,
+    color: "grey",
+  },
+  threadowner: {
+    color: "red",
+    textDecorationLine: "underline",
+  },
+  viewdetails: {
+    padding: 4,
+    backgroundColor: "#f4f5fb",
+    borderRadius: 5,
+    marginHorizontal: 5,
+  },
+  responsecontainer: {
+    backgroundColor: "white",
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+  },
+});
+
+
+// import { View, Text, StyleSheet} from 'react-native'
+// import React from 'react'
+// import { TouchableOpacity } from 'react-native-gesture-handler'
+// import { useNavigation } from '@react-navigation/native'
+// import { COLORS } from '../../constants/Theme'
+
+// const Threadsavailable = () => {
+//   const navigation = useNavigation();
+//   const Handlenavigatetocreatethread = () =>{
+//     navigation.navigate('Createthread')
+//   }
+//   return (
+//     <View style={{flex:1 ,alignItems:'center',justifyContent:'center'}}>
+//       <TouchableOpacity onPress={Handlenavigatetocreatethread} style={styles.nav} >
+//         <Text style={{color:'white',fontSize:16,fontWeight:500}}>Create your First Thread</Text>
+//       </TouchableOpacity>
+//     </View>
+//   )
+// }
+
+// export default Threadsavailable
+// const styles= StyleSheet.create({
+//   nav:{
+//     paddingHorizontal:20,
+//     paddingVertical:10,
+//     backgroundColor:COLORS.primary,
+//     borderRadius:10
+//   }
+// })
